788b9e88c04f6a3a308d38622f896a0b6b3f5e8b
Joost van Ulden <joost.vanulden@canada.ca>
2020-03-31 17:06:48 -0700

    Moved components into WET Grid

--- src/App.js
+++ src/App.js
@@ -83,16 +83,36 @@ class App extends Component {
     }
     
     return (
-      <div className="infoViz">
-        {/* {mapComponent}
-        <div className="narrative">
-          <h2>{title}</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      // <div className="infoViz">
+      //   {mapComponent}
+      //   <div className="narrative">
+      //     <h2>{title}</h2>
+      //     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      //   </div>
+      //   <div className="chart">
+      //     <Chart chart={chart} chartData={chartData} />
+      //   </div>
+      // </div>
+      
+      <div class="row">
+        <div class="col-md-8">
+          {mapComponent}
         </div>
-        <div className="chart">
-          <Chart chart={chart} chartData={chartData} />
-        </div> */}
+        <div class="col-md-4">
+          <div>
+            <div className="narrative">
+              <h2>{title}</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+          <div>
+            <div className="chart">
+              <Chart chart={chart} chartData={chartData} />
+            </div>
+          </div>
+        </div>
       </div>
+       
     )
   }
 }
